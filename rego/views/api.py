318d5fe03577bcff61e735a04112938285667839
Justin Steward <althalus87@gmail.com>
2014-11-17 12:13:03 +1100

    APIs

--- rego/views/api.py
+++ rego/views/api.py
@@ -1,7 +1,8 @@
 from flask.ext.restful import Api, Resource
 from rego.models import db, User, Device
-from rego.forms import UserForm
+from rego.forms import UserForm, SignupForm
 from flask import request
+import datetime
 
 
 class SignupAPI(Resource):
@@ -10,16 +11,39 @@ class SignupAPI(Resource):
         if form.validate_on_submit():
             user = User()
             form.populate_obj(user)
+            user.hash_password()
+            user.registration_date = datetime.datetime.now()
             db.session.add(user)
             db.session.commit()
+        else:
+            return {'errors': form.errors}, 406
 
 
 class LoginAPI(Resource):
-    pass
+    def post(self):
+        form = SignupForm()
+        if form.validate_on_submit():
+            user = User.query.filter_by(username=form.username.value).first()
+            if user.check_password(form.password):
+                device = Device()
+                device.user = user
+                device.registration_id = form.device_id
+                device.registration_date = datetime.datetime.now()
+                db.session.add(device)
+                db.session.commit()
+
+            return {'errors': 'Bad username or password'}, 401
+        return {'errors': form.errors}, 406
 
 
 class CheckAPI(Resource):
-    pass
+    def get(self):
+        device = Device.query.filter_by(registration_id=request.params.get('device')).first()
+        if not device:
+            return {'errors': 'Device not registered'}, 401
+        user = device.user
+        if user.account_status != User.STATUS_ACTIVE:
+            return {'errors': 'Account not active'}, 401
 
 
 api = Api(prefix='/api')
