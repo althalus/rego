79dd8a767e8d535bcd5ade1552c8771de0872007
Justin Steward <althalus87@gmail.com>
2014-11-17 11:55:56 +1100

    enough settings for dev

--- rego/settings.py
+++ rego/settings.py
@@ -4,3 +4,9 @@ class Config(object):
 
 class ProdConfig(object):
     pass
+
+
+class DevConfig(object):
+    DEBUG = True
+    SECRET_KEY = 'nosecret'
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///dev.db'
